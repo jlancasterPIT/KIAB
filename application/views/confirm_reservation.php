226da93bf891e2f6cb6c3d6af70df66b6c26e905
illum <jaydlancaster@gmail.com>
2013-11-21 16:32:49 -0500

    Added the confirm reservation

---
+++ application/views/confirm_reservation.php
@@ -0,0 +1,13 @@
+      <link rel="stylesheet" type="text/css" media="all" href="css/bootstrap-datetimepicker.min.css" />
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+      
+      <script type="text/javascript" src="js/bootstrap-datetimepicker.min.js"></script>
+
+      <!-- Jumbotron -->
+      <div class="jumbotron">
+        <div class="row">
+          <div class="col-md-12">
+	    <p class="lead">We have confirmed your reservation for <?php $reservationArray['dropoffDate']; ?>! See you then!</p>
+          </div>
+        </div>
+      </div>
