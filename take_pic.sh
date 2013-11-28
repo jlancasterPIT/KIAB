44bbcf46fb05d08413c356a1e6cea74b6d1352bc
illum <jaydlancaster@gmail.com>
2013-11-27 19:13:56 -0500

    Removing more ugc

--- take_pic.sh
+++
@@ -1,3 +0,0 @@
-v4lctl bright 85% && fswebcam -r 640x480 --jpeg 85 image0.jpeg -d /dev/video0 
-fswebcam -c /home/illum/.fswebcam.conf 
-#v4lctl contrast 100% && streamer -f jpeg -o image.jpeg -s 1024x768
