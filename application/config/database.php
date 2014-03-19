cfd8fd3e45d2317a5f7df7fca814e664617a43c9
Jeremy Lancaster <jeremy@eurekaranch.com>
2014-03-18 20:50:34 -0400

    Changing SQL stuff

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'lovemykids15401';
-$db['default']['database'] = 'kiabReservations';
+$db['default']['password'] = 'swordfish15401';
+$db['default']['database'] = 'kiabReservations2';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
