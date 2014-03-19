3d642f8e7a77d9d69d8de8b52c4bbe7ce6966cc2
Jeremy Lancaster <jeremy@eurekaranch.com>
2014-03-19 18:58:55 -0400

    Renamed a file

--- application/libraries/CmsSpotsIntegration.php
+++ application/libraries/CmsSpotsIntegration.php
@@ -7,7 +7,7 @@
 
   		$ci =& get_instance();
   		$ci->load->database();
-  		$query = $ci->db->query('SELECT * FROM cms_spots WHERE page=`'.$page.'`');
+  		$query = $ci->db->query('SELECT * FROM cms_spots WHERE page=\''.$page.'\'');
 
   		foreach ($query->result() as $row) {
   		  $data[$row->key] = $row->data;
