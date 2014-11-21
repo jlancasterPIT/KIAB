c54d4bd7dd4f03aee7db02ebc72497d0335960dc
Jeremy Lancaster <JeremyLancaster@brandingbrand.com>
2014-11-21 16:32:56 -0500

    Removing old views

--- application/views/login.php
+++
@@ -1,7 +0,0 @@
-<form action="/admin/login" method="post">
-	<label for="username">Username</label>
-	<input type="text" name="username" id="username" />
-	<label for="password">Password</label>
-	<input type="password" name="password" id="password" />
-	<input type="submit" name="submit" value="Login" />
-</form>
